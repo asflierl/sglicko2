180582e923d46b460d12266fc0995d990416fcca
Andreas Flierl <andreas@flierl.eu>
2022-05-29 20:45:01 +0200

    Add operator syntax for games

--- src/main/scala/sglicko2/WinOrDraw.scala
+++ src/main/scala/sglicko2/WinOrDraw.scala
@@ -2,6 +2,8 @@
 
 package sglicko2
 
+import scala.annotation.targetName
+
 enum WinOrDraw[A]:
   case Win(winner: A, loser: A)
   case Draw(player1: A, player2: A)
@@ -13,3 +15,11 @@ object WinOrDraw:
       case Draw(player1, player2) => (player1, player2, Score[0.5d]))
 
     override def toString = "Either one player wins or the game is a draw."
+
+  object Ops:
+    extension [A](a: A)
+      @targetName("win") def :>:(b: A) = Win(a, b)
+      infix def winsVs(b: A) = Win(a, b)
+      
+      @targetName("draw") def :=:(b: A) = Draw(a, b)
+      infix def tiesWith(b: A) = Draw(a, b)
--- src/test/scala/example/usage.scala
+++ src/test/scala/example/usage.scala
@@ -2,20 +2,20 @@
 
 package example
 
-import sglicko2.*
+import sglicko2.*, WinOrDraw.Ops.*
 
 @main def run: Unit = 
   given glicko2: Glicko2[String, WinOrDraw] = Glicko2()
 
-  import glicko2.*, WinOrDraw.*
+  import glicko2.*
 
   newLeaderboard
     .after(newRatingPeriod.withGames(
-      Win("Nilin", "Bob"),
-      Draw("Bob", "Nilin")))
+      "Nilin"   :>: "Bob",
+      "Bob"     :=: "Nilin"))
     .after(newRatingPeriod.withGames(
-      Win("Nilin", "Cookies"),
-      Win("Cookies", "Bob"),
-      Draw("Nilin", "Cookies")))
+      "Nilin"   :>: "Cookies",
+      "Cookies" :>: "Bob",
+      "Nilin"   :=: "Cookies"))
     .rankedPlayers
     .foreach(p => println(f"${p.rank}%2d ${p.player.id}%8s: [${p.player.confidence95.lower.value}%4.0f, ${p.player.confidence95.upper.value}%4.0f]"))
